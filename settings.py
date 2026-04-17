58b4995b3b7b7812513f7192bac22ee673080ac1
Abubakar Oluwatobi S <drtee909@gmail.com>
2026-04-17 16:35:46 +0100

    Update database password in settings.py

--- settings.py
+++ settings.py
@@ -58,7 +58,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get('DB_NAME', 'django_app'),
             'USER': os.environ.get('DB_USER', 'postgres'),
-            'PASSWORD': os.environ.get('DB_PASSWORD', '506050'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
             'HOST': os.environ.get('DB_HOST', 'localhost'),
             'PORT': os.environ.get('DB_PORT', '5432'),
         }
